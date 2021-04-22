902f2e6a5fe658d913605491adecc6cc705b88de
Shipeng Wang <wangshipeng8128@gmail.com>
2021-04-22 16:05:52 +0800

    Update task_list.py

--- HyperAdam/task_list.py
+++ HyperAdam/task_list.py
@@ -32,41 +32,4 @@ tasks = {
         'use_avg_loss': True,
         'training_lr': 1e-4,
     },
-    'HyperAdamVarSingle': {
-        'optimizer': HyperAdamVarSingle,
-        'meta_train_set': meta_train_set,
-        'meta_test_set': meta_test_set,
-        'use_avg_loss': True,
-        'training_lr': 1e-4,
-    },
-
-    'HyperAdamVarSingleNorm': {
-        'optimizer': HyperAdamVarSingleNorm,
-        'meta_train_set': meta_train_set_scale,
-        'meta_test_set': meta_test_set,
-        'use_avg_loss': True,
-        'training_lr': 1e-4,
-    },
-    ########Ablation Study########
-    'HyperAdamVarNeither': {
-        'optimizer': HyperAdamVar,
-        'meta_train_set': meta_train_set_neither,
-        'meta_test_set': meta_test_set,
-        'use_avg_loss': True,
-        'training_lr': 1e-4,
-    },
-    'HyperAdamVarConvex': {
-        'optimizer': HyperAdamVar,
-        'meta_train_set': meta_train_set_convex,
-        'meta_test_set': meta_test_set,
-        'use_avg_loss': True,
-        'training_lr': 1e-4,
-    },
-    'HyperAdamVarScale': {
-        'optimizer': HyperAdamVar,
-        'meta_train_set': meta_train_set_scale,
-        'meta_test_set': meta_test_set,
-        'use_avg_loss': True,
-        'training_lr': 1e-4,
-    },
 }
